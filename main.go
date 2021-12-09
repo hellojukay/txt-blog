0eae8a04b71ca9fe51ad33d47819c75b855b16ab
hellojukay <licong@qianxin.com>
2021-12-09 12:57:56 +0800

    update

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"errors"
 	"flag"
 	"fmt"
@@ -12,6 +13,11 @@ import (
 	"strings"
 )
 
+//go:embed custom.js
+var script string
+
+//go:embed custom.css
+var style string
 var bind = flag.String("bind", "127.0.0.1:8000", "port to run the server on")
 var dir = flag.String("dir", ".", "server root directory")
 
@@ -24,9 +30,6 @@ func main() {
 	log.Println("Serving on http://" + *bind)
 	log.Fatal(http.ListenAndServe(*bind, handler))
 }
-
-// 匹配 markdown 中的图片 ![img](src/img/big.png)
-var re = regexp.MustCompile(`!.*?\((.*?)\)`)
 
 type renderer struct {
 	d http.Dir
@@ -51,9 +54,47 @@ func (r renderer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		log.Printf("Couldn't read path %s: %v (%T)", req.URL.Path, err, err)
 		return
 	}
+	var title = get_title(string(input))
+	var head = fmt.Sprintf(`
+<title>%s</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/gml.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script>
+<style>%s</style>
+`, title, style)
+	var output = render_code(string(input))
 	// 展示图片
-	var output = re.ReplaceAllString(string(input), `</pre><img src="$1" style="max-width:100%%;max-height: 600px;" /><pre>`)
 
-	output = fmt.Sprintf("<html><body><pre>%s</pre></html></body>", output)
-	fmt.Fprintf(rw, output)
+	output = render_img(output)
+
+	output = fmt.Sprintf("<html><head>%s</head><body><div><pre>%s</pre></div></body><script>%s</script></html>", head, output, script)
+	fmt.Fprint(rw, output)
+}
+
+// 渲染代码块
+func render_code(html string) string {
+
+	var re = regexp.MustCompile("(?s)```([a-z]+)\r?\n(.*?)\r?\n```")
+	html = re.ReplaceAllString(html, `</pre><pre><code class="language-$1">$2</code></pre><pre>`)
+	re = regexp.MustCompile("(?s)```\r?\n(.*?)\r?\n```")
+	return re.ReplaceAllString(html, `</pre><pre><code class="language-plaintext">$1</code></pre><pre>`)
+}
+
+func render_link(html string) string {
+	// 匹配 markdown 中的图片 ![img](src/img/big.png)
+	var re = regexp.MustCompile(`\[.*?\]\((.*?)\)`)
+	return re.ReplaceAllString(html, `</pre><a href="$1"/>$1</a><pre>`)
+}
+func render_img(html string) string {
+	// 匹配 markdown 中的图片 ![img](src/img/big.png)
+	var re = regexp.MustCompile(`!.*?\((.*?)\)`)
+	return re.ReplaceAllString(html, `</pre><img src="$1"/><pre>`)
+
+}
+func get_title(html string) string {
+	var re = regexp.MustCompile(`title:\s?(.*?)\r?\n`)
+	var res = re.FindAllStringSubmatch(html, -1)
+	if len(res) > 0 {
+		return res[0][1]
+	}
+	return ""
 }
